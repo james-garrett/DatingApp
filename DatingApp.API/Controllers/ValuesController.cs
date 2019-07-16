42ba4f0bc0336df969fc18bdb9d441ada5a9a6ac
James <jkgarrett92@gmail.com>
2019-07-16 15:00:38 +1000

    Inital Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public ValuesController(DataContext context) 
+        {
+            _context = context;
+        }
+        
+        // GET api/values
+        // http://localhost:5000/api/values
+        // REST API uses http verbs as instrucions
+        [HttpGet]
+
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            // Returns 200 response with values
+            return Ok(values);
+        }
+
+        // GET api/values/id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            // FirstOrDefault returns null if the specific value isn't found, instead of undefined
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        // { get; set; } automatically adds getters and setters
+        public int Id { get; set; }
+    
+        public string name { get; set;}
+    }
+}
